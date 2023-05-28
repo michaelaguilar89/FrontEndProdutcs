5c41b680f67ef476238bf22da0533017fd33213b
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-05-27 20:58:17 -0600

    adding header and table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GetProductsComponent } from './components/get-products/get-products.component';
 import { CreateProductsComponent } from './components/create-products/create-products.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GetProductsComponent,
-    CreateProductsComponent
+    CreateProductsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
