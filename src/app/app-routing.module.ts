aa7f2fdac043e77e6479d4ad5e0978ede36fe801
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-05-27 20:43:59 -0600

    adding route on routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GetProductsComponent } from './components/get-products/get-products.component';
+import { CreateProductsComponent } from './components/create-products/create-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:GetProductsComponent},
+  {path:'create',component:CreateProductsComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
