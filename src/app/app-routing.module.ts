b6f018077fe5bac637ba50b4df06d350acecfdd6
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-05-28 16:17:05 -0600

    adding routerLink on update button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, createComponent } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GetProductsComponent } from './components/get-products/get-products.component';
 import { CreateProductsComponent } from './components/create-products/create-products.component';
@@ -6,6 +6,9 @@ import { CreateProductsComponent } from './components/create-products/create-pro
 const routes: Routes = [
   {path:'',component:GetProductsComponent},
   {path:'create',component:CreateProductsComponent},
+  {path:'update/:id',component:CreateProductsComponent},
+  {path:'details/:id',component:CreateProductsComponent},
+  {path:'remove/:id',component:CreateProductsComponent}
   
 ];
 
--- src/app/components/get-products/get-products.component.ts
+++ src/app/components/get-products/get-products.component.ts
@@ -1,5 +1,6 @@
 
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { ProductServiceService } from 'src/app/services/product-service.service';
 
 @Component({
@@ -11,7 +12,9 @@ export class GetProductsComponent {
 
  
   myResponse:any=[];
-  constructor(public service:ProductServiceService){
+  constructor(public service:ProductServiceService,
+              private router:Router    
+      ){
 
   }
   
@@ -22,5 +25,7 @@ export class GetProductsComponent {
     })
   
   }
-
+  update(id:string){
+    this.router.navigate(['/update/'+id]);
+  }
 }
